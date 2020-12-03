27aed4e952f9012722d351db0f43af908e3a3a88
swa06 <leowashfi@gmail.com>
2020-12-03 20:46:24 +0600

    Take photo using camera or pick from gallery in AddNotesActivity

---
+++ app/src/main/java/com/washfi/todoapp/activity/AddNotesActivity.kt
@@ -0,0 +1,175 @@
+package com.washfi.todoapp.activity
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
+import com.washfi.todoapp.BuildConfig
+import com.washfi.todoapp.R
+import com.washfi.todoapp.utils.AppConstant
+import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class AddNotesActivity : AppCompatActivity() {
+    lateinit var editTextTitle: EditText
+    lateinit var editTextDescription: EditText
+    lateinit var submitButton: Button
+    lateinit var imageViewAdd: ImageView
+    val REQUEST_CODE_GALLERY = 2
+    val REQUEST_CODE_CAMERA = 3
+    val REQUEST_PERMISSION_CODE = 124
+    var picturePath = ""
+    lateinit var imageLocation:File
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notes)
+        bindView()
+        addClickListener()
+    }
+
+    private fun addClickListener() {
+        submitButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val intent = Intent()
+                intent.putExtra(AppConstant.TITLE, editTextTitle.text.toString())
+                intent.putExtra(AppConstant.DESCRIPTION, editTextDescription.text.toString())
+                intent.putExtra(AppConstant.IMAGE_PATH, picturePath)
+
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        })
+
+        imageViewAdd.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                if (checkAndRequestPermission())
+                    setUpDialog()
+            }
+
+        })
+    }
+
+    private fun checkAndRequestPermission(): Boolean {
+        val permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val permissionStorage = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        val listOfPermissionNeeded = ArrayList<String>()
+        if (permissionStorage != PackageManager.PERMISSION_GRANTED) {
+            listOfPermissionNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED) {
+            listOfPermissionNeeded.add(Manifest.permission.CAMERA)
+        }
+
+        if (listOfPermissionNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, listOfPermissionNeeded.toTypedArray<String>(),
+                    REQUEST_PERMISSION_CODE)
+            return false
+        }
+
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setUpDialog()
+                }
+            }
+        }
+    }
+
+    private fun setUpDialog() {
+        val view = LayoutInflater.from(this@AddNotesActivity).inflate(R.layout.dialog_selector, null)
+        val textViewCamera = view.findViewById<TextView>(R.id.textViewCamera)
+        val textViewGallery = view.findViewById<TextView>(R.id.textViewGallery)
+        val dialog = AlertDialog.Builder(this)
+                .setView(view)
+                .setCancelable(true)
+                .create()
+
+        textViewCamera.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if (takePictureIntent.resolveActivity(packageManager) != null) {
+                    var photoFile: File? = null
+                    try {
+                        photoFile = createImageFile()
+                    } catch (e: Exception) {
+
+                    }
+                    if (photoFile != null) {
+                        val photoUri = FileProvider.getUriForFile(this@AddNotesActivity,
+                                BuildConfig.APPLICATION_ID + ".provider", photoFile)
+                        imageLocation = photoFile
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                        dialog.hide()
+                        startActivityForResult(takePictureIntent, REQUEST_CODE_CAMERA)
+                    }
+                }
+            }
+        })
+
+        textViewGallery.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, REQUEST_CODE_GALLERY)
+                dialog.hide()
+            }
+        })
+
+        dialog.show()
+    }
+
+    private fun createImageFile(): File? {
+        val timeStamp = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        val fileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDir)
+    }
+
+    private fun bindView() {
+        editTextTitle = findViewById(R.id.editTextTitle)
+        editTextDescription = findViewById(R.id.editTextDescription)
+        submitButton = findViewById(R.id.buttonSubmit)
+        imageViewAdd = findViewById(R.id.imageViewAdd)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_CAMERA -> {
+                    picturePath = imageLocation.path.toString()
+                    Glide.with(this).load(imageLocation.absoluteFile).into(imageViewAdd)
+                }
+                REQUEST_CODE_GALLERY -> {
+                    val selectedImage = data?.data
+                    picturePath = selectedImage.toString()
+                    Glide.with(this).load(picturePath).into(imageViewAdd)
+                }
+            }
+        }
+    }
+}
