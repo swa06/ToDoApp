6d3e128f50f005c2d664d3d8f51cc19f90efa7cf
swa06 <leowashfi@gmail.com>
2020-12-05 20:06:40 +0600

    Show foreground and background notification from fcm

--- app/src/main/java/com/washfi/todoapp/activity/SplashActivity.kt
+++ app/src/main/java/com/washfi/todoapp/activity/SplashActivity.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.washfi.todoapp.utils.PrefConstant
 import com.washfi.todoapp.R
 
@@ -16,6 +20,23 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         setUpSharedPreferences()
         checkLoginStatus()
+        getFcmToken()
+    }
+
+    private fun getFcmToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("SplashActivity", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("SplashActivity", token.toString())
+            Toast.makeText(baseContext, token.toString(), Toast.LENGTH_SHORT).show()
+        })
     }
 
     private fun setUpSharedPreferences() {
---
+++ app/src/main/java/com/washfi/todoapp/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,44 @@
+package com.washfi.todoapp.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.washfi.todoapp.R
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d("FirebaseMessagService", message?.from.toString())
+        Log.d("FirebaseMessagService", message?.notification?.body.toString())
+        setUpNotification(message)
+    }
+
+    private fun setUpNotification(message: RemoteMessage) {
+        val channelId = getString(R.string.notification_channel_id)
+        val ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(message?.notification?.title)
+                .setContentText(message?.notification?.body)
+                .setSound(ringtone)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Todo-Notes", NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
