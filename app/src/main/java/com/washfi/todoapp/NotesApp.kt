e767dd2a72c8cc5ec2a11f5568a5bdbb31fd2567
swa06 <leowashfi@gmail.com>
2020-12-14 21:42:51 +0600

    Initialize AndroidNetworking instance in NotesApp

--- app/src/main/java/com/washfi/todoapp/NotesApp.kt
+++ app/src/main/java/com/washfi/todoapp/NotesApp.kt
@@ -1,11 +1,13 @@
 package com.washfi.todoapp
 
 import android.app.Application
+import com.androidnetworking.AndroidNetworking
 import com.washfi.todoapp.db.NotesDatabase
 
 class NotesApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        AndroidNetworking.initialize(applicationContext)
     }
 
     fun getNotesDb(): NotesDatabase {
