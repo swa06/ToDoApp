85062b0c9715409157fa077650d18bec1d56b9b4
swa06 <leowashfi@gmail.com>
2020-12-25 17:06:18 +0600

    Redesign SplashActivity and add functionality

--- app/src/main/java/com/washfi/todoapp/splash/SplashActivity.kt
+++ app/src/main/java/com/washfi/todoapp/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.washfi.todoapp.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,13 +16,22 @@ import com.washfi.todoapp.onboarding.OnboardingActivity
 import com.washfi.todoapp.data.local.pref.StoreSession
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var handler:Handler
+    lateinit var runnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setUpSharedPreferences()
-        checkLoginStatus()
-        getFcmToken()
+        goToNext()
+    }
+
+    private fun goToNext() {
+        handler = Handler()
+        runnable = Runnable {
+            checkLoginStatus()
+        }
+        handler.postDelayed(runnable, 2000)
     }
 
     private fun getFcmToken() {
@@ -61,5 +71,10 @@ class SplashActivity : AppCompatActivity() {
             }
         }
         finish()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        handler.removeCallbacks(runnable)
     }
 }
